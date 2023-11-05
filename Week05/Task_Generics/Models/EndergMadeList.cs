4a349c2b22947e7f96b36a8d98ff317290fa6cef
MustafazadeJafar <mi625nkm8@code.edu.az>
2023-11-05 23:34:29 +0400

    + Week05 task "Generics".

---
+++ Week05/Task_Generics/Models/EndergMadeList.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Task_Generics.Models;
+
+public class EndergMadeList<T1>
+{
+    T1[] _array;
+
+    public int RealSize { get => this._array.Length; }
+    public int Capacity { get; }
+    public int Length { get; private set; } //Count
+    public T1 this[int i]
+    {
+        get
+        {
+            if (i < this.Length && i >= 0)
+            {
+                return this._array[i];
+            }
+            else throw new ArgumentOutOfRangeException();
+        }
+        set
+        {
+            if (i < this.Length && i >= 0)
+            {
+                this._array[i] = value;
+            }
+            else throw new ArgumentOutOfRangeException();
+        }
+    }
+
+
+    public EndergMadeList(int capacity = 0)
+    {
+        if (capacity < 2) capacity = 6;
+        this.Capacity = capacity;
+        this.Length = 0;
+        this._array = new T1[0];
+    }
+    public EndergMadeList(in T1[] array, int capacity = 0) : this(capacity)
+    {
+        this.Length = array.Length;
+        this._array = new T1[(array.Length / this.Capacity + 1) * this.Capacity];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            this._array[i] = array[i];
+        }
+    }
+
+
+    public void Add(T1 element)
+    {
+        if (this.Length == this._array.Length)
+        {
+            Array.Resize(ref this._array, this.Length + this.Capacity);
+        }
+        this._array[this.Length++] = element;
+    }
+    public void Clear()
+    {
+        this.Length = 0;
+        this._array = new T1[0];
+    }
+    public bool Contains(T1 element)
+    {
+        if (this.IndexOf(element) >= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool Remove(int index, bool WithException = false)
+    {
+        if (index >= this.Length || index < 0)
+        {
+            // actual purpose of bool variable is to differ "Remove(int index)" from "Remove(int element)".
+            if (WithException) throw new ArgumentOutOfRangeException();
+            return false;
+        }
+        T1[] newArray = new T1[this._array.Length];
+
+        for (int i = 0; i < index;)
+        {
+            newArray[i] = this._array[i++];
+        }
+        this.Length--;
+
+        for (int i = index; i < this.Length;)
+        {
+            newArray[i] = this._array[++i];
+        }
+        this._array = newArray;
+
+        return true;
+    }
+    public bool Remove(T1 element)
+    {
+        int index = this.IndexOf(element);
+        if (index >= 0)
+        {
+            this.Remove(index, false);
+            return true;
+        }
+        return false;
+    }
+
+    public T1[] Reverse()
+    {
+        T1[] newArray = new T1[this.Length];
+        for (int i = 0; i < this.Length; i++)
+        {
+            newArray[this.Length - i - 1] = this._array[i];
+        }
+
+        return newArray;
+    }
+
+    public int IndexOf(T1 element)
+    {
+        for (int i = 0; i < this.Length; i++)
+        {
+            if (this._array[i].Equals(element))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int LastIndexOf(T1 element)
+    {
+        for (int i = this.Length - 1; i >= 0; i--)
+        {
+            if (this._array[i].Equals(element))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Week05/Task_Generics/Program.cs
@@ -0,0 +1,48 @@
+﻿using Task_Generics.Models;
+
+namespace Task_Generics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        EndergMadeList<int> array1 = new EndergMadeList<int>(new int[] { 0, -10, -4, -9 }, -4);
+
+        Console.WriteLine("Capacity: " + array1.Capacity);
+        for (int c = 0; c < 4; c++)
+        {
+            array1[c] = -c;
+            array1.Add(c * c);
+            Console.WriteLine("Size: " + array1.RealSize);
+        }
+
+        Console.WriteLine("\nSuccsess of remove is... " + array1.Remove(array1.IndexOf(-1), default));
+        for (int i = 0; i < array1.Length; i++)
+        {
+            Console.Write(array1[i] + ", ");
+        }
+
+        Console.WriteLine("\n\nSuccsess of remove is... " + array1.Remove(-3));
+        for (int i = 0; i < array1.Length; i++)
+        {
+            Console.Write(array1[i] + ", ");
+        }
+
+        Console.WriteLine("\n\nIndex of last 0 is: " + array1.LastIndexOf(0));
+
+        Console.WriteLine("\nThis is how reverse would look like: ");
+        foreach (var item in array1.Reverse())
+        {
+            Console.Write(item + ", ");
+        }
+
+        array1.Clear();
+        Console.WriteLine("\n\nsize after clear: " + array1.RealSize + "; as well as length: " + array1.Length);
+
+        if (!array1.Contains(0))
+        {
+            Console.WriteLine("\nBoom Shakalaka");
+            array1[0] = 0;
+        }
+    }
+}
